23aa449d41ef020b80088ab3a28d32ff70475eae
ramer-dev <gml4009@gmail.com>
2023-06-08 18:24:35 +0900

    배포용 상대경로 지정 완료

--- yolo_v8/eval.py
+++ yolo_v8/eval.py
@@ -10,15 +10,21 @@
 #     print(output)
 
 if __name__ == '__main__':
-    import torch
+    import torch, os
     from ultralytics import YOLO
-    model = YOLO('C:/Users/gml40/PycharmProjects/Flight-Visualizator-OCR-Model/yolo_v8/runs/detect/train/weights/last.pt')
+
+    model = YOLO(os.path.join(os.getcwd(), 'runs', 'detect', 'train', 'weights', 'last.pt'))
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device)
 
     # result = model("C:/Users/gml40/PycharmProjects/Flight-Visualizator-OCR-Model/yolo_v8/dataset/valid/images/0_png.rf.3f6808bef1fb25a1f114cadd717ed5e8.jpg")
-    results = model(source="C:/Users/gml40/PycharmProjects/Flight-Visualizator-OCR-Model/yolo_v8/dataset/valid/images/0_png.rf.6b29f3242f1617147db1b5da409a057d.jpg",
-                        show=True, show_labels=True)
+
+    file_name = '0_png.rf.6b29f3242f1617147db1b5da409a057d.jpg'
+    file_path = os.path.join(os.getcwd(), 'dataset', 'valid', 'images', file_name)
+
+    results = model(
+        source=file_path,
+        show=True, show_labels=True)
 
     print(results[0].boxes.boxes)
 
